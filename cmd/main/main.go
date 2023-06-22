3401041f3230150d448572e2d2cd1d7955212679
Rajender <rajenderkatkuri123@gmail.com>
2023-06-23 01:23:17 +0530

    feat: movie api

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/rajenderK7/go-mysql/pkg/database"
+	"github.com/rajenderK7/go-mysql/pkg/routes"
+	"github.com/rajenderK7/go-mysql/pkg/utils"
+)
+
+var (
+	DBName     string
+	DBUser     string
+	DBPassword string
+	DBPORT     string
+)
+
+func loadENV() {
+	err := godotenv.Load("../../.env")
+	utils.CheckGeneralErr(err)
+	DBName = os.Getenv("DB_NAME")
+	DBUser = os.Getenv("DB_USER")
+	DBPassword = os.Getenv("DB_PASSWORD")
+	DBPORT = os.Getenv("DB_PORT")
+}
+
+func main() {
+	app := *fiber.New()
+	loadENV()
+	config := database.DBConfig{
+		DBName:     DBName,
+		DBUser:     DBUser,
+		DBPassword: DBPassword,
+		DBPORT:     DBPORT,
+	}
+	database.ConnectDB(&config)
+	database.RunMigrations()
+	routes.SetupRoutes(&app)
+	log.Fatal(app.Listen(":4000"))
+}
